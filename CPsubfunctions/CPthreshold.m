2929d369ecaa2db1da6470856d0b6c0daf79dd8f
HACKERMD <markusdherrmann@gmail.com>
2015-04-10 14:28:48 +0200

    Fix bugs
    These bugs may be syntax errors only in newer versions of Matlab.
    For me these lines broke 'matlab.codetools.requiredFilesAndProducts()'

--- CPsubfunctions/CPthreshold.m
+++ CPsubfunctions/CPthreshold.m
@@ -292,8 +292,10 @@ elseif strcmp(Threshold,'All')
             %%% Retrieves the path where the images are stored from the
             %%% handles structure.
             fieldname = ['Pathname', ImageName];
-            try Pathname = handles.Pipeline.(fieldname);
-            catch error(['Image processing was canceled in the ', ModuleName, ' module because it must be run using images straight from a load images module (i.e. the images cannot have been altered by other image processing modules). This is because you have asked the Identify Primary Threshold module to calculate a threshold based on all of the images before identifying objects within each individual image as CellProfiler cycles through them. One solution is to process the entire batch of images using the image analysis modules preceding this module and save the resulting images to the hard drive, then start a new stage of processing from this Identify Primary Threshold module onward.'])
+            try 
+                Pathname = handles.Pipeline.(fieldname);
+            catch
+                error(['Image processing was canceled in the ', ModuleName, ' module because it must be run using images straight from a load images module (i.e. the images cannot have been altered by other image processing modules). This is because you have asked the Identify Primary Threshold module to calculate a threshold based on all of the images before identifying objects within each individual image as CellProfiler cycles through them. One solution is to process the entire batch of images using the image analysis modules preceding this module and save the resulting images to the hard drive, then start a new stage of processing from this Identify Primary Threshold module onward.'])
             end
             %%% Retrieves the list of filenames where the images are stored
             %%% from the handles structure.
