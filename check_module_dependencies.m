3388a21285a392c3d059aeb92bdef74f18859772
HACKERMD <markusdherrmann@gmail.com>
2015-04-22 01:03:43 +0200

    Fix writing file into temporary directory

--- check_module_dependencies.m
+++ check_module_dependencies.m
@@ -15,22 +15,19 @@ function check_module_dependencies(local_CP_dir, local_lib_dir)
 % Author:
 %   Markus Herrmann (2015)
 
-% Create backup of current path
-p = path;
-
-% Before you run the dependency check,
-% clear your path and then only add the path to the local copies of
-% - 'CellProfilerPelkmans'
-% - 'PelkmansLibrary'
-% repositories.
-fprintf('. Restoring default Matlab path\n')
-restoredefaultpath;
-fprintf('. Defining locations of repositories and setting up Matlab path for dependency checks\n')
-addpath(genpath(local_CP_dir));
-addpath(genpath(local_lib_dir));
-
 % import packages required for this script
 import util.json.*
+
+% Create backup of current path variable
+p = path;
+
+% Before running the dependency check,
+% clear your path and then only add the path to the local copies of repositories.
+fprintf('. Restoring default Matlab path\n')
+restoredefaultpath;
+fprintf('. Defining repository locations and setting up Matlab path for dependency checks\n')
+addpath(genpath(local_CP_dir));
+addpath(genpath(local_lib_dir));
 
 % create a list of CPP modules
 folder_content = dir(fullfile(local_CP_dir, 'Modules'));
@@ -50,8 +47,8 @@ end
 path(p);
 
 % write the complete dependency list to temporary file as JSON
-output_file = fullfile(tempname, 'CPmodule_dep.json');
-fprintf('. List of module dependencies will be written to the follwing file:\n%s\n', output_file)
+output_file = fullfile(tempdir, 'CPmodule_dep.json');
+fprintf('. List of module dependencies will be written to the following file:\n%s\n', output_file)
 fclose('all');  % safety first!
 fid = fopen(output_file, 'w');
 fprintf(fid, util.json.savejson('', dep));
