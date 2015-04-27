aa477a998005a796a38b6dc1aded6d0f84925e16
Owen Feehan <owenfeehan@users.noreply.github.com>
2015-04-27 15:19:14 +0200

    Updated tests

--- Tests/runAllTests.m
+++ Tests/runAllTests.m
@@ -17,9 +17,9 @@ runner = TestRunner.withTextOutput;
 
 tapFile = '../testsOutput.tap';
 
-# We delete the existing tapFile as TAPPlugin.producingOriginalFormat
-#    appends to any existing files, and we need it to be only a s
-#    single test case for Jenkins.
+% We delete the existing tapFile as TAPPlugin.producingOriginalFormat
+%   appends to any existing files, and we need it to be only a s
+%    single test case for Jenkins.
 delete(tapFile);
 plugin = TAPPlugin.producingOriginalFormat(ToFile(tapFile));
 
