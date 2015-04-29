8b5da98ba9af1d14e80cd2c6ec99b8e3f0110c3d
Owen Feehan <owenfeehan@users.noreply.github.com>
2015-04-29 13:52:16 +0200

    Make all dummy tests pass

--- Scripts/ExampleTest.m
+++ Scripts/ExampleTest.m
@@ -1,7 +1,7 @@
 classdef ExampleTest < matlab.unittest.TestCase
     methods(Test)
         function dummyTestOne(testCase)  % Test fails
-            testCase.verifyEqual(5, 4, 'Testing 5==4')
+            testCase.verifyEqual(5, 5, 'Testing 5==5')
         end
         function dummyTestTwo(testCase)  % Test passes
             testCase.verifyEqual(5, 5, 'Testing 5==5')
