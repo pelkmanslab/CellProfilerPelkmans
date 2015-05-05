7c5532a34f7e9a0d4229f939d9706274ac8304cb
Owen Feehan <owenfeehan@users.noreply.github.com>
2015-05-05 18:40:55 +0200

    Added IlluminationCorrectionZScoringTest

---
+++ Modules/Tests/IlluminationCorrectionZScoringTest.m
@@ -0,0 +1,38 @@
+classdef IlluminationCorrectionZScoringTest < matlab.unittest.TestCase
+    
+    properties
+        A1_HandlesIn
+        A1_HandlesOut
+    end
+ 
+    methods(TestMethodSetup)
+        function createHandles(testCase)
+            dirTestsSub = 'State/IlluminationCorrectionZScoring';
+            
+            pathOld = 'S:\Data\Users\Owen\TestDatasets\Dataset 3\TIFF';
+            pathNew = absolutePathFromRelative('Input/A/TIFF');
+            
+            %testCase.A1_HandlesIn = getRewrittenTestHandles(dirTestsSub,'A/1_handles_in', pathOld, pathNew);
+            %testCase.A1_HandlesOut = getRewrittenTestHandles(dirTestsSub,'A/1_handles_out', pathOld, pathNew);
+            
+            testCase.A1_HandlesIn = getTestHandles(dirTestsSub,'A/1_handles_in');
+            testCase.A1_HandlesOut = getTestHandles(dirTestsSub,'A/1_handles_out');
+
+        end
+    end
+ 
+    methods(TestMethodTeardown)
+        function closeHandles(testCase)
+            testCase.A1_HandlesIn = [];
+            testCase.A1_HandlesOut = [];
+        end
+    end
+    
+    methods(Test)
+        function handlesMatchA(testCase)
+            testResult = IlluminationCorrectionZScoring( testCase.A1_HandlesIn );
+            testCase.verifyTrue( isequal(testResult, testCase.A1_HandlesOut) );
+        end
+    end
+
+end
