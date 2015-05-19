7804f14dff0f5992b6bed6c56a303d46d3dc7a19
Owen Feehan <owenfeehan@users.noreply.github.com>
2015-05-19 14:35:12 +0200

    Fixed test-problems. Refactored test code.

--- ModuleTests/DiscardObjectsBySizeTest.m
+++ ModuleTests/DiscardObjectsBySizeTest.m
@@ -1,29 +1,20 @@
 classdef DiscardObjectsBySizeTest < matlab.unittest.TestCase
-    
-    properties
-        C1_HandlesIn
-        C1_HandlesOut
-    end
  
     methods(TestMethodSetup)
-        function createHandles(testCase)
-            dirTestsSub = 'State/DiscardObjectsBySize';
-            testCase.A1_HandlesIn = getTestHandles(dirTestsSub,'C/1_handles_in');
-            testCase.A1_HandlesOut = getTestHandles(dirTestsSub,'C/1_handles_out');
-        end
     end
  
     methods(TestMethodTeardown)
-        function closeHandles(testCase)
-            testCase.A1_HandlesIn = [];
-            testCase.A1_HandlesOut = [];
-        end
     end
     
     methods(Test)
         function handlesMatchC(testCase)
-            testResult = DiscardObjectsBySize( testCase.C1_HandlesIn );
-            testCase.verifyTrue( isequal(testResult, testCase.C1_HandlesOut) );
+            
+            dirTestsSub = 'State/DiscardObjectsBySize';
+            handlesIn = getTestHandles(dirTestsSub,'C/1_handles_in');
+            handlesOut = getTestHandles(dirTestsSub,'C/1_handles_out');
+            
+            testResult = DiscardObjectsBySize( testCase.handlesIn );
+            testCase.verifyTrue( isequal(testResult, testCase.handlesOut) );
         end
     end
 end
--- ModuleTests/IlluminationCorrectionPelkmansTest.m
+++ ModuleTests/IlluminationCorrectionPelkmansTest.m
@@ -1,33 +1,29 @@
 classdef IlluminationCorrectionPelkmansTest < matlab.unittest.TestCase
     
     properties
-        A1_HandlesIn
-        A1_HandlesOut
+        handlesIn
+        handlesOut
     end
  
     methods(TestMethodSetup)
-        function createHandles(testCase)
+    end
+ 
+    methods(TestMethodTeardown)
+    end
+    
+    methods(Test)
+        function handlesMatchA(testCase)
+            
             dirTestsSub = 'State/IlluminationCorrectionZScoring';
             
             pathOld = 'S:\Data\Users\Owen\TestDatasets\Dataset 3';
             pathNew = absolutePathFromRelative('Input/A');
             
-            testCase.A1_HandlesIn = getRewrittenTestHandles(dirTestsSub,'A/1_handles_in', pathOld, pathNew);
-            testCase.A1_HandlesOut = getRewrittenTestHandles(dirTestsSub,'A/1_handles_out', pathOld, pathNew);
-        end
-    end
- 
-    methods(TestMethodTeardown)
-        function closeHandles(testCase)
-            testCase.A1_HandlesIn = [];
-            testCase.A1_HandlesOut = [];
-        end
-    end
-    
-    methods(Test)
-        function handlesMatchA(testCase)
-            testResult = IlluminationCorrectionPelkmans( testCase.A1_HandlesIn );
-            testCase.verifyTrue( isequal(testResult, testCase.A1_HandlesOut) );
+            handlesIn = getRewrittenTestHandles(dirTestsSub,'A/1_handles_in', pathOld, pathNew);
+            handlesOut = getRewrittenTestHandles(dirTestsSub,'A/1_handles_out', pathOld, pathNew);
+            
+            testResult = IlluminationCorrectionPelkmans( handlesIn );
+            testCase.verifyTrue( isequal(testResult, handlesOut) );
         end
     end
 
